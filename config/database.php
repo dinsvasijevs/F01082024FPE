6911977ca15f670deea6a4f1e1a172c499a39428
DeanVas <batman1913@inbox.lv>
2024-08-06 18:15:51 +0300

    Added currency database changes

--- config/database.php
+++ config/database.php
@@ -30,12 +30,14 @@ return [
     */
 
     'connections' => [
+
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('C:/Users/dinae/Herd/database.sqlite')),
+            'url' => env('DB_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
-            'foreign_key_constraints' => false,
-            'busy_timeout' => 5000,
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'busy_timeout' => null,
             'journal_mode' => null,
             'synchronous' => null,
         ],
