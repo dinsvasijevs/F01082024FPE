6cc754a6af072cf54e6bee180d2f55e918721338
DeanVas <batman1913@inbox.lv>
2024-08-06 17:57:45 +0300

    Added currency database path

--- config/database.php
+++ config/database.php
@@ -30,14 +30,12 @@ return [
     */
 
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE', database_path('C:/Users/dinae/Herd/database.sqlite')),
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => null,
+            'foreign_key_constraints' => false,
+            'busy_timeout' => 5000,
             'journal_mode' => null,
             'synchronous' => null,
         ],
